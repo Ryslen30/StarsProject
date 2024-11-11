cb06e97542328e12a50f14b84d0349b998400360
nour-big-brain <nourbouraoui99@gmail.com>
2024-11-11 16:47:14 +0100

    fixed header and added login signup contact

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,18 +6,18 @@ import { NgModule } from '@angular/core';
 import { AdminComponent } from './components/admin/admin.component';
 import { ErrorComponent } from './components/error/error.component';
 import { StarsListComponent } from './components/stars-list/stars-list.component';
-import { StarItemComponent } from './components/star-item/star-item.component';
-import { StarSelectedComponent } from './components/star-selected/star-selected.component';
-import { CartComponent } from './components/cart/cart.component';
-
+import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
+import { ForgotpasswordComponent } from './components/forgotpassword/forgotpassword.component';
 export const routes: Routes = [
   {path :'home', component: HomeComponent},
   { path: 'about', component: AboutComponent }, // Route for About
   { path: 'contact', component: ContactComponent }, // Route for Contact
-  {path: 'stars', component: StarsListComponent }, // Route for star
-  {path: 'cart', component: CartComponent }, 
-  {path: 'stars/:id', component: StarSelectedComponent }, 
+  {path: 'stars', component: StarsListComponent }, // Route for stars
   {path:'rynou' , component: AdminComponent}, // Route for administration
+  {path:'login',component: LoginComponent}, // Route for login
+  {path:'signup',component: SignupComponent}, // Route for signup
+  // {path:'forgotpassword',component: ForgotPasswordComponent}, // Route for forgot passwordng
   { path: '', component: HomeComponent }, 
   {path :'**', component: ErrorComponent},
   
